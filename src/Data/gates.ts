943f802ef92fe41a7e0c15caffe847095fbc8ca0
Matias <none>
2021-01-31 09:17:26 -0600

    update flightengine again

--- src/Data/gates.ts
+++ src/Data/gates.ts
@@ -1,19 +1,39 @@
-const gates: Gate[] = [];
-
-const letters = 'ABCDEFGH';
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
-
-// add gates to array
-for (let i = 0; i < letters.length; i += 1) {
-  for (let j = 0; j < numbers.length; j += 1) {
-    gates.push({
-      location: {
-        latitude: 41.9742,
-        longitude: 87.9073,
-      },
-      code: letters[i] + numbers[j],
-    } as Gate);
+export default [
+  {
+    code: 'H18',
+    location: { latitude: 41.97294279723098, longitude: -87.90025406851116 }
+  }, {
+    code: 'H12',
+    location: { latitude: 41.9735024220196, longitude: -87.90047103450748 }
+  }, {
+    code: 'G20',
+    location: { latitude: 41.97314509468786, longitude: -87.90333219466999 }
+  }, {
+    code: 'G5',
+    location: { latitude: 41.97446634841843, longitude: -87.90317063187388 }
+  }, {
+    code: 'E14',
+    location: { latitude: 41.974609194887385, longitude: -87.90901584084787 }
+  }, {
+    code: 'B2',
+    location: { latitude: 41.97716261146444, longitude: -87.90725285538188 }
+  }, {
+    code: 'C3',
+    location: { latitude: 41.976794707479385, longitude: -87.91066336117039 }
+  }, {
+    code: 'C32',
+    location: { latitude: 41.98089336826694, longitude: -87.90977822691755 }
+  }, {
+    code: 'B18',
+    location: { latitude: 41.98139246301013, longitude: -87.90622019001236 }
+  }, {
+    code: 'B7',
+    location: { latitude: 41.978465965691946, longitude: -87.90697585450671 }
+  }, {
+    code: 'B1',
+    location: { latitude: 41.977062902670674, longitude: -87.90713496692001 }
+  }, {
+    code: 'K1',
+    location: { latitude: 41.97674199042463, longitude: -87.90044707329064 }
   }
-}
-
-export default gates;
+] as Gate[];
